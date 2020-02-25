1179937db88eec187b816609f78e6c4ba449a5e2
mattyeo <matteoleo@gmail.com>
2020-02-25 19:47:03 +0100

    Init basic components

---
+++ src/app/user-repos/components/user-repos-search-bar/user-repos-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-repos-search-bar',
+  templateUrl: './user-repos-search-bar.component.html',
+  styleUrls: ['./user-repos-search-bar.component.sass']
+})
+export class UserReposSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user-repos/user-repos.container.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-repos',
+  templateUrl: './user-repos.container.html',
+  styleUrls: ['./user-repos.container.sass']
+})
+export class UserReposContainer implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
