58dbe476971e8bdb8f50f69d069efcabb1293de5
mattyeo <matteoleo@gmail.com>
2020-03-01 14:31:42 +0100

    User Repos Retriever Implementation

---
+++ src/app/user-repos/services/user-repos.config.ts
@@ -0,0 +1,2 @@
+export const API_GIT_USER_URL = 'https://api.github.com/users/';
+export const API_GIT_REPOSITORY_ENDING = '/repos';
--- src/app/user-repos/user-repos.module.ts
+++ src/app/user-repos/user-repos.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import {UserReposContainer} from './user-repos.container';
 import {UserReposSearchResultListComponent} from './components/user-repos-search-result-list/user-repos-search-result-list.component';
 import {UserReposSearchBarComponent} from './components/user-repos-search-bar/user-repos-search-bar.component';
+import {UserReposRetrieverService} from './services/user-repos-retriever/user-repos-retriever.service';
 import {UserReposRoutingModule} from './user-repos-routing.module';
-import { UserReposSearchResultItemComponent } from './components/user-repos-search-result-item/user-repos-search-result-item.component';
+import {UserReposSearchResultItemComponent} from './components/user-repos-search-result-item/user-repos-search-result-item.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
-  imports: [UserReposRoutingModule],
+  imports: [UserReposRoutingModule, HttpClientModule],
   declarations: [
     UserReposContainer,
     UserReposSearchBarComponent,
     UserReposSearchResultListComponent,
     UserReposSearchResultItemComponent
-  ]
+  ],
+  providers: [UserReposRetrieverService]
 })
 export class UserReposModule { }
